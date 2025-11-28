4fa1952183af5b8cac118006c584f534fd598e85
arshad <mohammedarshad@jmbrgroup.com>
2025-11-28 16:36:00 +0400

    Add feature to retrieve rental companies
    Introduced a new feature to fetch rental companies from the database and expose them via an API endpoint.
    
    - Added `GetRentalCompanies` method to `IRentalCompanyBl` and `IRentalCompanyBr` interfaces.
    - Implemented `GetRentalCompanies` in `RentalCompanyBl` and `RentalCompanyBr` using Dapper for SQL execution.
    - Created `RentalCompanyResponse` DTO for rental company data structure.
    - Added HTTP GET endpoint in `RentalCompanyController`.
    - Added SQL query in `GetRentalCompanies.sql` and managed it via resource files.
    - Updated `DriveSync.csproj` to include new resources.

--- DriveSync/DriveSync/BusinessLogic/IBusinesLogic/IRentalCompanyBl.cs
+++ DriveSync/DriveSync/BusinessLogic/IBusinesLogic/IRentalCompanyBl.cs
@@ -1,9 +1,11 @@
 ﻿using DriveSync.DTOS.Request;
+using DriveSync.DTOS.Response;
 
 namespace DriveSync.BusinessLogic.IBusinesLogic
 {
     public interface IRentalCompanyBl
     {
         Task<string> Register(RegisterCompaniesRequest request);
+        Task<List<RentalCompanyResponse>> GetRentalCompanies();
     }
 }
---
+++ DriveSync/DriveSync/DTOS/Response/RentalCompanyResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace DriveSync.DTOS.Response
+{
+    public class RentalCompanyResponse
+    {
+        public long Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public long ContactNumber { get; set; }
+        public DateTimeOffset CreatedDateTime { get; set; }
+        public DateTimeOffset UpdatedDateTime { get; set; }
+        public short IsActive { get; set; }
+        public string? Remarks { get; set; }
+    }
+}
